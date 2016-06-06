34ce8c99f6850ede07f2ccbb6f04d21335a59aa2
Radakovic <radakovicradoslav@gmail.com>
2016-06-06 17:06:16 +0200

    GMA-5068 Marketing pages - Home page

---
+++ app/assets/js/scripts.min.js
@@ -0,0 +1,14 @@
+/*!
+ * GoMathGo
+ * Catch Up, Keep Up, or Get Ahead with Go Math! Academy!
+ * https://www.gomathacademy.com/
+ * @author HMH GMA Development Team
+ * @version 1.0.0
+ * Copyright 2016. MIT licensed.
+ */
+// jshint unused:false
+(function ($, window, document) {
+
+  'use strict';
+
+})(jQuery, window, document);
